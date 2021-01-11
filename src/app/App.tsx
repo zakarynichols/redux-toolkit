867f9064c074fdff79b151d814ee1653e0b8de65
Zakary Nichols <zakaryn@VTDEV-iMac.local.tld>
2021-01-11 08:17:42 -0500

    created reducer and slice for issues display

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from './rootReducer';
+import {
+    displayRepo,
+    setCurrentDisplayType,
+    setCurrentPage
+} from 'features/issuesDisplay/issuesDisplaySlice';
 
 const App: React.FC = () => {
+    const dispatch = useDispatch();
+
+    const { org, repo, displayType, page, issueId } = useSelector(
+        (state: RootState) => state.issuesDisplay
+    );
+
+    const setOrgAndRepo = (org: string, repo: string) => {
+        dispatch(displayRepo({ org, repo }));
+    };
+
+    const setJumpToPage = (page: number) => {
+        dispatch(setCurrentPage(page));
+    };
+
+    const showIssuesList = () => {
+        dispatch(setCurrentDisplayType({ displayType: 'issues' }));
+    };
+
+    const showIssueComments = (issueId: number) => {
+        dispatch(setCurrentDisplayType({ displayType: 'comments', issueId }));
+    };
+
     return <div>Live reload</div>
-}
+};
 
 export default App;
